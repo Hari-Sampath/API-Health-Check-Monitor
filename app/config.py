3664317a778bb52fc46d90cc178656284ab0e71b
Hari-Sampath <harisampathbellam@gmail.com>
2026-05-03 19:07:30 +0530

    Added code for config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,16 @@
+import yaml
+
 json_data = open("apis.yaml", "r")
 yaml_file = yaml.safe_load(json_data)
+
+global_settings = yaml_file["settings"]  # global settings
 api_info = yaml_file["apis"]  # list with information of apis
 
 
-names = [api["name"] for api in api_info]
+def get_api_detail(api_name, detail):
+    for api in api_info:
+        if api["name"] == api_name:
+            return api[detail]
+
+
+print(get_api_detail("HTTPBin GET", str(input("enter what detail you want : "))))
--- app/main.py
+++ app/main.py
@@ -1,11 +1,4 @@
+import config
 import network
-import yaml
-
-json_data = open("apis.yaml", "r")
-yaml_file = yaml.safe_load(json_data)
-api_info = yaml_file["apis"]  # list with information of apis
-
-
-names = [api["name"] for api in api_info]
-
-print(names)
+import storage
+import validator
