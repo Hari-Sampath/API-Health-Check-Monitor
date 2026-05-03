8b4604995b93c95464d86581479f43e482003ad4
Hari-Sampath <harisampathbellam@gmail.com>
2026-05-03 18:55:15 +0530

    created a defined structure for files

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
+import network
 import yaml
 
 json_data = open("apis.yaml", "r")
 yaml_file = yaml.safe_load(json_data)
 api_info = yaml_file["apis"]  # list with information of apis
+
 
 names = [api["name"] for api in api_info]
 
