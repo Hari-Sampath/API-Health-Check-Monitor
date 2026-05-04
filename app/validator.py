e902fa5f2cc3e05859b977abc0925d25c687c108
Hari-Sampath <harisampathbellam@gmail.com>
2026-05-04 23:00:24 +0530

    Updated Validator.py (second iteration))

--- app/validator.py
+++ app/validator.py
@@ -1,21 +1,47 @@
 def validate(responses, api_info):
+
     report = []
+    expected_time = []
+    expected_status = []
+    response_contains = []
+    j = 0
+
     for api in api_info:
         expected = api["expected"]
 
-        expected_time = expected["response_time_ms"]
-        expected_status = expected["status"]
+        expected_time.append(expected["response_time_ms"])
+        expected_status.append(expected["status"])
+        if "response_contains" in expected:
+            response_contains.append(expected["response_contains"])
+        else:
+            response_contains.append("No Params")
 
-        for response in responses:
-            if response["time"] > expected_time:
-                speed = "slower than expected"
-            else:
-                speed = "healthy"
-            if expected_status == response["response_code"]:
-                health = "Healthy"
-            else:
-                health = "Faulty"
-            valid = 1
-            report.append({"health": health, "speed": speed, "valid": valid})
+    for response in responses:
+        if response["time"] > expected_time[j]:
+            speed = "Slower than expected"
+        else:
+            speed = "Expected speed"
+
+        if expected_status[j] == response["response_code"]:
+            health = "Healthy"
+        else:
+            health = "Faulty"
+
+        if response_contains == "No params":
+            valid = "Accurate response provided"
+        elif "response_contains" in response["output"]:
+            valid = "Accurate response provided"
+        else:
+            valid = "Inaccurate response provided"
+
+        report.append(
+            {
+                "Name": response["api_name"],
+                "health": health,
+                "speed": speed,
+                "valid": valid,
+            }
+        )
+    j += 1
 
     return report
