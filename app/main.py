801cb1613f00fe7c7181dd942d7bea7c95facb1b
Hari-Sampath <harisampathbellam@gmail.com>
2026-05-05 08:49:31 +0530

    Updated main.py

--- app/main.py
+++ app/main.py
@@ -9,5 +9,14 @@ with open("apis.yaml", "r") as yaml_data:
 global_settings = yaml_file["settings"]  # global settings
 api_info = yaml_file["apis"]  # list with information of apis
 
+
 responses = network.get_status(api_info, global_settings)
 print(result)
+
+responses = network.get_status(
+    api_info, global_settings
+)  # sending api request and fetching response
+
+report = validator.validate(responses, api_info)  # checking validity of response
+
+print(responses[0:2])
