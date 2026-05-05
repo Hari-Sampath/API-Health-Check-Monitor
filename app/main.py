9c04a27836ce037040a529c524ee2a573e27647f
Hari-Sampath <harisampathbellam@gmail.com>
2026-05-05 09:11:23 +0530

    Updated alerts.py (second iteration)

--- app/main.py
+++ app/main.py
@@ -1,23 +1,21 @@
-import network
-import storage
-import validator
 import yaml
+from alerts import generate_alerts
+from network import get_status
+from validator import validate
 
 
 def main():
+
     with open("apis.yaml", "r") as yaml_data:
         yaml_file = yaml.safe_load(yaml_data)
 
     global_settings = yaml_file["settings"]  # global settings
     api_info = yaml_file["apis"]  # list with information of apis
 
-    responses = network.get_status(
-        api_info, global_settings
-    )  # sending api request and fetching response
-
-    report = validator.validate(responses, api_info)  # checking validity of response
-
-    print(responses[0:2])
+    responses = get_status(api_info, global_settings)  # sending api request and fetching response # fmt: skip
+    report = validate(responses, api_info)  # checking validity of response
+    issues = generate_alerts(report)
+    print(issues)
 
 
 if __name__ == "__main__":
